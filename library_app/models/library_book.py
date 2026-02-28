0af5aeb53dc0bb7a3ff77b81dcdf3ef275851172
Cristian Rodier <crodier@protonmail.com>
2026-02-28 07:55:22 -0600

    create a one-to-many relationship between book and the publisher

--- library_app/models/library_book.py
+++ library_app/models/library_book.py
@@ -1,6 +1,5 @@
 from odoo import fields, models
 from odoo.exceptions import ValidationError
-from dateutil.relativedelta import relativedelta
 
 class Book(models.Model):
     _name = "library.book"
